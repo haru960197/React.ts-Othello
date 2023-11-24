ad85737d7f38a79d9a5b9bcbce92b3adeff25e14
Haruki Kurokawa <ha.brack96@gmail.com>
2023-11-24 12:44:05 +0900

    [fix] index.cssをインポート

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import './theme/index.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
